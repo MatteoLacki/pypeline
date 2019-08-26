8579f7ef25442e0044bd15d7dbd803b1fe65b009
MatteoLacki <matteo.lacki@gmail.com>
2019-08-26 13:33:34 +0200

    added timings. need to make it less klamoten-like.

--- vodkas/examples/proteome_tools_pool_2.py
+++ vodkas/examples/proteome_tools_pool_2.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import json
 from collections import defaultdict
 import subprocess
+from time import time
 
 from vodkas import apex3d, StdErr, peptide3d, iadbs, wx2csv
 
@@ -23,6 +24,9 @@ if debug:
 
 exceptions = defaultdict(list)
 
+
+
+
 # rawdatapath, fastapath = settings[2]
 # data = settings[2:3]
 # data = settings[2:]
@@ -36,6 +40,7 @@ data = [(r,f) for r,f in settings if Path(r).stem not in analysed]
 assert {Path(r).stem for r,f in data}.intersection(analysed) == set([])
 # rawdatapath, fastapath = data[0]
 # data = [data[0]]
+timings = {}
 
 for rawdatapath, fastapath in data:
     OK = True
@@ -50,13 +55,16 @@ for rawdatapath, fastapath in data:
         print('out_folder', '\n\t', out_folder)
         print('parameters_file','\n\t', parameters_file)
         print('timeout\n\t{} h'.format(timeout/3600))
+    timings[raw_folder] = {}
     try:
+        t_apex = time()
         apexOut, apex_proc = apex3d(rawdatapath,
                                     out_folder,
                                     write_binary=True,
                                     capture_output=capture_output,
                                     debug=debug,
                                     timeout=timeout)
+        timings[raw_folder]['apex3d'] = t_apex = time() - t_apex
         if debug:
             print(apexOut, apex_proc)
     except subprocess.TimeoutExpired:
@@ -70,12 +78,14 @@ for rawdatapath, fastapath in data:
         OK = False
     if OK:
         try:
+            t_pep3d = time()
             pep3dOut, pep_proc = peptide3d(apexOut.with_suffix('.bin'),
                                            out_folder,
                                            write_binary=True,
                                            min_LEMHPlus=350.0,
                                            capture_output=capture_output,
                                            debug=debug)
+            timings[raw_folder]['peptide3d'] = t_pep3d = time() - t_pep3d
             if debug:
                 print(pep3dOut, pep_proc)
         except Exception as e:
@@ -87,12 +97,14 @@ for rawdatapath, fastapath in data:
             OK = False
     if OK:
         try:
+            t_iadbs = time()
             iadbsOut, iadbs_proc = iadbs(pep3dOut.with_suffix('.xml'),
                                          out_folder, 
                                          fasta_file=fastapath,
                                          parameters_file=parameters_file,
                                          capture_output=capture_output,
                                          debug=debug)
+            timings[raw_folder]['iadbs'] = t_iadbs = time() - t_iadbs
             if debug:
                 print(iadbsOut, iadbs_proc)
         except Exception as e:
@@ -106,15 +118,21 @@ for rawdatapath, fastapath in data:
     # out_folder= Path(r"D:/projects/proteome_tools/RES/T1707/T170722_03")
     if OK:
         try:
+            t_wx2csv = time()
             report, wx2csv_proc = wx2csv(iadbsOut.with_suffix('.xml'),
                                          out_folder/"report.csv",
                                          debug=debug)
+            timings[raw_folder]['wx2csv'] = t_wx2csv = time() - t_wx2csv
             if debug:
                 print(report, wx2csv_proc)
                 print("Finished")
         except Exception as e:
             print(e)
             exceptions.append(e)
+
+timings_path = Path("D:/projects/proteome_tools/RES/pool2/timings.json")
+with open(timings_path, 'w') as f:
+    json.dump(timings, f, indent=4)
 
 print("And now all the exceptions:")
 print(exceptions)
