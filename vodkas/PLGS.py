77dba797a3ec42d39a84400ea305673545eb361c
MatteoLacki <matteo.lacki@gmail.com>
2019-08-30 14:07:50 +0200

    Changed the rules for preparing proper folders.

--- vodkas/PLGS.py
+++ vodkas/PLGS.py
@@ -11,8 +11,32 @@ from vodkas.xml_parser import parse_xmls
 def _2xml(p):
     return p.with_suffix('.xml')
 
+
 def _2bin(p):
     return p.with_suffix('.bin')
+
+
+def get_paths(raw_folder, out_folder, network_out_folder):
+    """Get proper names for the folders.
+
+    Checks, if the folder already exists somewhere.
+    Maybe it's stupid. Maybe it should check if it existed before?
+    """
+    raw = Path(raw_folder)
+    out = Path(out_folder)
+    net_out = Path(network_out_folder)
+    proj_tag = raw.name[:5]
+    if proj_tag[0] in ('O','I'):
+        used = {p.name for x in (out, net_out) for p in x.glob(proj_tag+'*')}
+        if used:
+            used_No = {int(t.split('_')[1]) for t in used if "_" in t}
+            if (out/proj_tag).exists() or (net_out/proj_tag).exists():
+                used_No.add(0)
+            proj_tag = "{}_{}".format(proj_tag, max(used_No) + 1)
+        out /= proj_tag
+        if net_out:
+            net_out /= proj_tag
+    return raw, out, net_out
 
 
 def plgs(raw_folder,
@@ -35,27 +59,22 @@ def plgs(raw_folder,
     Returns:
         dict: parsed parameters from the xml files.
     """
-    raw = Path(raw_folder)
-    out = Path(out_folder)
-    net_out = Path(network_out_folder)
-    proj_tag = raw.name[:5] # I1907, O1908, ...
-    if proj_tag[0] in ('O','I'):
-        out /= proj_tag
-        if net_out:
-            net_out /= proj_tag
+    raw, out, net_out = get_paths(raw_folder, out_folder, network_out_folder)
     fas = fastas(proteome, **kwds)
     par_f = Path(parameters_file) # 215.xml, ...
     T = {}
     a_p, _, T['apex3d'] = apex3d(raw, out, **kwds)
     p_p, _, T['pep3d'] = peptide3d(_2bin(a_p), out, **kwds)
     i_p, _, T['iadbs'] = iadbs(_2xml(p_p), out, fas, par_f,**kwds)
-
     xml_params, params = parse_xmls(a_p, p_p, i_p)
+
     with open(out/'params.json', 'w') as f:
         json.dump(params, f, indent=2) # for projectizer2.0
-
     if net_out:
-        copy_folder(out, net_out/proj_tag)
+        copy_folder(out, net_out)
 
-    xml_params['pep3d']['fasta'] = fas
+    xml_params['apex3d']['local_raw_folder'] = str(out)
+    xml_params['apex3d']['server_raw_folder'] = str(net_out)
+    xml_params['peptide3d']['fasta'] = str(fas)
+    
     return xml_params, T
--- vodkas/tests/test_plgs.py
+++ vodkas/tests/test_plgs.py
@@ -1,6 +1,7 @@
 from vodkas.examples.PLGS import PLGS
 from vodkas.xml_parser import parse_xmls
 from pprint import pprint
+from pathlib import Path
 
 PLGS("C:/ms_soft/MasterOfPipelines/Data/O190302_01.raw", "human")
 
@@ -12,3 +13,46 @@ pprint(xml_params['apex3d'])
 pprint(xml_params['apex3d'])
 
 
+proj_tag = "O1903"
+p = Path()
+len(list(p.parent.glob(proj_tag)))
+
+
+# there is a problem
+
+raw_folder = 'C:/SYMPHONY_VODKAS/temp/O1903'
+out_folder = 'C:/SYMPHONY_VODKAS/temp'
+network_out_folder = 'J:/test_RES'
+
+
+raw_folder = 'C:/SYMPHONY_VODKAS/temp/O1904'
+out_folder = 'C:/SYMPHONY_VODKAS/temp'
+network_out_folder = 'J:/test_RES'
+
+raw = Path(raw_folder)
+out = Path(out_folder)
+net_out = Path(network_out_folder)
+proj_tag = raw.name[:5]
+
+
+def get_paths(raw_folder, out_folder, network_out_folder):
+    """Get proper names for the folders.
+
+    Checks, if the folder already exists somewhere.
+    Maybe it's stupid. Maybe it should check if it existed before?
+    """
+    raw = Path(raw_folder)
+    out = Path(out_folder)
+    net_out = Path(network_out_folder)
+    proj_tag = raw.name[:5]
+    if proj_tag[0] in ('O','I'):
+        used = {p.name for x in (out, net_out) for p in x.glob(proj_tag+'*')}
+        if used:
+            used_No = {int(t.split('_')[1]) for t in used if "_" in t}
+            if (out/proj_tag).exists() or (net_out/proj_tag).exists():
+                used_No.add(0)
+            proj_tag = "{}_{}".format(proj_tag, max(used_No) + 1)
+        out /= proj_tag
+        if net_out:
+            net_out /= proj_tag
+    return raw, out, net_out
