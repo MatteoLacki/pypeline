af748487578a18b3adafb0569d0a113264b93129
MattLacki <matteo.lacki@gmail.com>
2019-09-30 13:19:42 +0200

    Debugging pipeline.

---
+++ vodkas/examples/project_O190906.py
@@ -0,0 +1,12 @@
+r"""Sabine's project.
+
+\\MSSERVER\obelix_rawdata\RAW\O1909
+File names O190906_02 to _26
+
+
+X:\SYMPHONY_VODKAS\fastas\custom\horny_mouse\20180515_up_mouse_reviewed_16970entries_and_171contaminants+hNECAB1+2_reverese.fasta
+"""
+import logging
+from pathlib import Path
+from pprint import pprint
+
