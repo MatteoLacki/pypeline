7f894e63cad34a84cbafe5bebe10c39c2833680a
matteo <matteo.lacki@gmail.com>
2019-11-12 12:57:27 +0100

    last changes to Thomas project.

--- vodkas/debug/copy_folder.py
+++ vodkas/debug/copy_folder.py
@@ -3,10 +3,9 @@ from pathlib import Path
 from vodkas.fs import copy_folder, find_free_path
 
 network_db_folder = Path(r"Y:/TESTRES")
-p = Path(r"O:\RAW\O1910\O191017_19.raw")
-r = Path(r'Y:/TESTRES/O191017_19')
+# p = Path(r"O:\RAW\O1910\O191017_19.raw")
+# r = Path(r'Y:/TESTRES/O191017_19')
 # copy_folder(p, r)
-
 
 acquired_name = "O191017_18"
 ##                              Y:\RES\         2019-008\   O191017-04
@@ -14,4 +13,4 @@ acquired_name = "O191017_18"
 ##                              Y:\RES\         O191017-04
 out_folder = Path("C:\\SYMPHONY_VODKAS\\temp\\2019-008\\O191017_18")
 net_folder = find_free_path(network_db_folder/acquired_name)
-copy_folder(out_folder, net_folder)
+s = copy_folder(out_folder, net_folder)
