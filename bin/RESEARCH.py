9d9e241aa393abefc49e566ae97888e7131912b1
MatteoLacki <matteo.lacki@gmail.com>
2020-03-04 14:31:51 +0100

    simplified researching.

--- bin/RESEARCH.py
+++ bin/RESEARCH.py
@@ -1,10 +1,11 @@
 import argparse
 from docstr2argparse.parse import foo2argparse
+import json
 import logging
 from pathlib import Path
 import platform
 from pprint import pprint
-import json
+from tqdm import tqdm
 
 from fs_ops.paths import find_suffixed_files
 
@@ -16,23 +17,41 @@ from vodkas.research import research
 ap = argparse.ArgumentParser(description='Rerun search with iaDBs.',
                              epilog="WARNING: PREVIOUS '*_IA_Workflow.xml' SHALL BE DELETED ",
                              formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# ap.add_argument('--prompt', action='store_true', help='Prompt users for input.')
 ap.add_argument('--log_file', type=lambda p: Path(p).expanduser().resolve(),
                 help='Path to temporary outcome folder.',
                 default={"Windows": 'C:/SYMPHONY_VODKAS/temp_logs/research.log',
                          "Linux":   '~/SYMPHONY_VODKAS/research.log',
                          "Darwin":  '~/SYMPHONY_VODKAS/research.log'}[platform.system()])
+
 ap.add_argument('--server_ip', type=str, help='IP of the server', default='0.0.0.0')
-for n,o,h in foo2argparse(get_fastas, get_short=False):
+
+get_fastas_kwds = foo2argparse(get_fastas, args_prefix='fastas_', get_short=False)
+for n,o,h in get_fastas_kwds:
+    if o == 'fastas_prompt':
+        h['action'] = "store_true"
     ap.add_argument(n, **h)
+
+iadbs_kwds = foo2argparse(iadbs, args_prefix='iadbs_', positional=False, get_short=False)
+for n,o,h in iadbs_kwds:
+    ap.add_argument('--iadbs_'+o,**h)
+
 ap.add_argument('Pep3D_Spectrum', type=Path, nargs='+',
     help="Path(s) to outputs of Peptide3D. \
           If provided with a folder instead, \
           a recursive search for files matching '*_Pep3D_Spectrum.xml' is performed.")
-iadbs_kwds = foo2argparse(iadbs, args_prefix='iadbs_', positional=False, get_short=False)
-for n,o,h in iadbs_kwds:
-    ap.add_argument('--iadbs_'+o,**h)
+
+
 args = ap.parse_args()
+pprint(args.__dict__)
+
 iadbs_kwds = {o: args.__dict__[n.replace('--','')] for n,o,h in iadbs_kwds}
+get_fastas_kwds = {o: args.__dict__[n.replace('--','')] for n,o,h in get_fastas_kwds}
+
+try: # translate fastas to NCBIgeneralFastas and store it on the server.
+    iadbs_kwds['fasta_file'] = get_fastas(**get_fastas_kwds)
+except FileNotFoundError:
+    log.error(f"Fastas unreachable: {fastas_path}")
 
 logging.basicConfig(filename=args.log_file,
                     format='%(asctime)s:%(name)s:%(levelname)s:%(message)s:', 
@@ -42,4 +61,21 @@ log = logging.getLogger('RESEARCH.py')
 xmls = list(find_suffixed_files(args.Pep3D_Spectrum, ['**/*_Pep3D_Spectrum.xml'], ['.xml']))
 print("analyzing folders:")
 pprint(xmls)
-research(xmls, args.fastas_path, iadbs_kwds, log, args.server_ip)
+
+# server = Sender('RESEARCH', server_ip)
+
+for xml in tqdm(xmls):
+    log.info(f"researching: {str(xml)}")
+    try:
+        iadbs_kwds['input_file'] = xml
+        iadbs_kwds['output_dir'] = xml.parent
+        # server.send_pair('iadbs_args', dump2json(iadbs_kwds))
+        iadbs_out, _, runtime = iadbs(**iadbs_kwds)
+        apex_out = iadbs_out.parent/iadbs_out.name.replace('_IA_workflow.xml', '_Apex3D.xml')
+        create_params_file(apex_out, xml, iadbs_out) # for projectizer2.0
+        search_stats = iaDBsXMLparser(iadbs_out).info()
+        # server.send_pair("stats", dump2json(search_stats))
+        rows2csv(iadbs_out.parent/'stats.csv', [list(search_stats), list(search_stats.values())])
+    except Exception as e:
+        log.warning(repr(e))
+log.info("Search redone.")
--- vodkas/fastas.py
+++ vodkas/fastas.py
@@ -4,45 +4,63 @@ from furious_fastas import fastas, Fastas
 from .fs import move
 
 
-def get_fastas(fastas_path,
-               fastas_db=r'X:/SYMPHONY_VODKAS/fastas/latest',
+def get_fastas(path_or_tag='none',
+               db=r'X:/SYMPHONY_VODKAS/fastas/latest',
                add_contaminants=True,
-               reverse_fastas=True):
+               reverse=True,
+               prompt=False):
     """Get proper fastas.
 
     Args:
-        fastas_path (str): path to fasta file or one of the standard proteomes used, e.g. 'human'.
-        fastas_db (str): Path to fastas DB: used when supplying reduced fasta names, e.g. 'human'.
+        path_or_tag (str): path to fasta file or one of the standard proteomes used, e.g. 'human'.
+        db (str): Path to fastas DB: used when supplying reduced fasta names, e.g. 'human'.
         add_contaminants (boolean): Should we add in contaminants.
-        reverse_fastas (boolean):Should we reverse the fastas.
+        reverse (boolean):Should we reverse the fastas.
+        prompt (boolean): Prompt users for input.
 
     Returns:
         Path: path to the fastas.
     """
-    standard_fastas = {p.stem.split('_')[0]:p for p in Path(fastas_db).glob(f"*/PLGS/*.fasta")}
-    if str(fastas_path) in standard_fastas:
-        outpath = standard_fastas[fastas_path]
+    standard_fastas = {p.stem.split('_')[0]:p for p in Path(db).glob(f"*/PLGS/*.fasta")}
+    if prompt:
+        path_or_tag = input('fastas to use (human|wheat|..|custom path): ')
+        if str(path_or_tag) in standard_fastas:
+            print(f"Selected: {path_or_tag}, stored under {standard_fastas[path_or_tag]}")
+        else:
+            print(f"Selected: {path_or_tag}")
+            add_contaminants = input('Adding contaminants: to stop me write "no": ')
+            add_contaminants = add_contaminants.lower() != 'no'
+            print(f'Contaminants: {add_contaminants}')
+            reverse = input('Reversing fastas: to stop me write "no": ')
+            reverse = reverse.lower() != 'no'
+            print(f'Reversing fastas: {reverse}')
     else:
-        fastas_path = Path(fastas_path)
-        if not fastas_path.exists():
-            raise FileNotFoundError
+        if path_or_tag == 'none':
+            raise FileNotFoundError('You did not specify a path for fastas.')
+
+    if str(path_or_tag) in standard_fastas:
+        outpath = standard_fastas[path_or_tag]
+    else:
+        path_or_tag = Path(path_or_tag)
+        if not path_or_tag.exists():
+            raise FileNotFoundError('Path does not exist.')
         # TODO: if path is there, don't do all that!
-        final_name = fastas_path.stem
+        final_name = path_or_tag.stem
         if add_contaminants:
             final_name += "_contaminated"
-        if reverse_fastas:
+        if reverse:
             final_name += "_reversed"
         final_name += "_pipelineFriendly.fasta"
-        outpath = fastas_path.parent/final_name
+        outpath = path_or_tag.parent/final_name
         if not outpath.exists():
-            fs = fastas(fastas_path)
+            fs = fastas(path_or_tag)
             if add_contaminants:
                 from furious_fastas.contaminants import contaminants
                 fs.extend(contaminants)
             fs_gnl = Fastas(f.to_ncbi_general() for f in fs)
             assert fs_gnl.same_fasta_types(), "Fastas are not in the same format."
-            if reverse_fastas:
+            if reverse:
                 fs_gnl.reverse()
-            outpath = fastas_path.parent/(fastas_path.stem + '_contaminated_reversed_pipelineFriendly.fasta')
+            outpath = path_or_tag.parent/(path_or_tag.stem + '_contaminated_reversed_pipelineFriendly.fasta')
             fs_gnl.write(outpath)
     return outpath
